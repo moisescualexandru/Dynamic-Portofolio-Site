e8093957044e48fd75d8e0b574519a6eb2e497ba
Alexandru Moisescu <moisescu.alexandru88@gmail.com>
2018-08-21 14:39:14 +0300

    Added blur effect for the panels

--- src/js/app.js
+++ src/js/app.js
@@ -4,20 +4,35 @@ function toggleOpen() {
   if(this.classList.contains('open')) {
     this.classList.remove('open');
     this.classList.remove('open-active');
+    this.classList.remove('unblur');
   } else {
     panels.forEach(panel => {
       panel.classList.remove('open');
       panel.classList.remove('open-active');
+      panel.classList.remove('unblur');
     })  
     this.classList.toggle('open');
+    this.classList.add('unblur');
   }
 }
 
 function toggleActive(e) {
   if (e.propertyName.includes('flex') && e.target.classList.contains('open')) {
     this.classList.add('open-active');
+    this.classList.add('unblur');
   } 
+}
+
+function clearBlur() {
+  this.classList.add('unblur');
+}
+
+function addBlur() {
+  if(this.classList.contains('open')) return;
+  this.classList.remove('unblur');
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+panels.forEach(panel => panel.addEventListener('mouseover', clearBlur));
+panels.forEach(panel => panel.addEventListener('mouseout', addBlur));
