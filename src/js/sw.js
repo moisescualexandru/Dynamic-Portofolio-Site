b2b738e6270f7529ed82832903b52152676c0ed3
Alexandru Moisescu <moisescu.alexandru88@gmail.com>
2018-08-23 12:05:01 +0300

    Fixed bug with the urls (missing'/')

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,26 +1,30 @@
-var CACHE_NAME = 'site-cache-v1';
+var namedCache = 'site-cache-v1';
 var urlsToCache = [
   '/',
-  'src/css/styles.css',
-  'src/js/app.js',
-  'src/img/About.png',
-  'src/img/Classic Arcade Game - blur-sm.jpg',
-  'src/img/Classic Arcade Game.png',
-  'src/img/Memory Game - blur-sm.jpg',
-  'src/img/Memory Game.png',
-  'src/img/myreads - blur-sm.jpg',
-  'src/img/myreads.png',
-  'src/img/Neighborhood Restaurant - blur-sm.jpg',
-  'src/img/Neighborhood Restaurant.png',
-  'src/img/ProfilePic.jpg'
+  '/src/css/styles.css',
+  '/src/js/app.js',
+  '/src/img/About.png',
+  '/src/img/Classic Arcade Game - blur-sm.jpg',
+  '/src/img/Classic Arcade Game.png',
+  '/src/img/Memory Game - blur-sm.jpg',
+  '/src/img/Memory Game.png',
+  '/src/img/myreads - blur-sm.jpg',
+  '/src/img/myreads.png',
+  '/src/img/Neighborhood Restaurant - blur-sm.jpg',
+  '/src/img/Neighborhood Restaurant.png',
+  '/src/img/ProfilePic.jpg'
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(namedCache)
           .then(function(cache) {
             console.log('Opened cache');
-            return cace.addAll(urlsToCache);
+            return cache.addAll(urlsToCache);
+          })
+          .catch(function(error) {
+            console.log(error);
+            return;
           })
   );
 });
@@ -35,6 +39,7 @@ self.addEventListener('fetch', function(event) {
             }
             return fetch(event.request);
           })
+          .catch(function(error) {console.log(error)})
   );
 });
 
@@ -44,13 +49,13 @@ self.addEventListener('activate', function(event) {
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
-      return Promis.all(
+      return Promise.all(
         cacheNames.map(function(cacheName) {
           if(cacheWhiteList.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
       );
-    })
+    }).catch(function(error) {console.log(error)})
   );
 });
